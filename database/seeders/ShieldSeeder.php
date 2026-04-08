fdc0a8d10f630d7d9efa394f33cdc2555a7b5f5a
Ezequiel Ardiles <ezequielardiles2005@gmail.com>
2026-04-08 17:43:47 -0400

    fix: corregir el manejo de permisos en ShieldSeeder y actualizar la asignación de roles

--- database/seeders/ShieldSeeder.php
+++ database/seeders/ShieldSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use BezhanSalleh\FilamentShield\Support\Utils;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\PermissionRegistrar;
 
 class ShieldSeeder extends Seeder
 {
@@ -14,12 +15,16 @@ class ShieldSeeder extends Seeder
      */
     public function run(): void
     {
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
+
         $this->command->info('Generando permisos y roles de Shield...');
         $this->command->call('shield:generate', [
             '--all' => true,
             '--panel' => 'admin',
             '--no-interaction' => true,
         ]);
+
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
 
         $this->command->info('Creando usuario Super Admin...');
 
@@ -31,7 +36,9 @@ class ShieldSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
 
-        $user->assignRole(Utils::getSuperAdminName());
+        $user->syncRoles([Utils::getSuperAdminName()]);
+
+        app(PermissionRegistrar::class)->forgetCachedPermissions();
 
         $this->command->info('¡Listo! Login: admin@admin.com | pass: password');
     }
